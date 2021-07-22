fd0eb6753b1ae321c3317e42a026df10145fb85d
IagoAguiar <iagoaguiar202@gmail.com>
2021-07-22 18:41:47 -0300

    Iniciando o projeto

---
+++ src/main/java/one/digitalInnovation/personapi/controller/PersonController.java
@@ -0,0 +1,21 @@
+package one.digitalInnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+	
+	@GetMapping
+	public String getBook() {
+		return "API Test!";
+	}
+	
+	
+	
+	
+}
