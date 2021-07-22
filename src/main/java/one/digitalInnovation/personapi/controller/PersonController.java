419a1ccf3e86e29973c8e313e2c513ecb0700a1c
IagoAguiar <iagoaguiar202@gmail.com>
2021-07-22 19:36:45 -0300

    Entidades criadas

--- src/main/java/one/digitalInnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalInnovation/personapi/controller/PersonController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalInnovation.personapi.entity.Person;
+
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -12,6 +14,10 @@ public class PersonController {
 	
 	@GetMapping
 	public String getBook() {
+		
+		Person person = new Person();
+		
+		person.getId();
 		return "API Test!";
 	}
 	
---
+++ src/main/java/one/digitalInnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,19 @@
+package one.digitalInnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+
+    
+    
+    
+}
